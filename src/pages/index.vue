64de5cb9c1faf69fba2350ef41dffb7523af5826
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-05-03 13:48:19 +0200

    ajout fichier pocketbase

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
-//
+  const maisonsListe: {
+    adresse: string;
+    collectionId: string;
+    /* ... */
+  }[] = /* ... */
+
+  interface MaisonRecord {
+  adresse: string;
+  collectionId: string;
+  /* ... */
+  }
+
+  const maisonsListe: MaisonRecord[] = /* ... */
 </script>
+
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
